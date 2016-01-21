8387b6eabb59b3626ffaa0f362faaa8b2e503f3b
WangBin <wangbin@ucredit.com>
2016-01-21 14:33:05 +0800

    使用guava，并补充部分注释

--- src/main/java/cn/binarywang/tools/generator/IDCardNumberGenerator.java
+++ src/main/java/cn/binarywang/tools/generator/IDCardNumberGenerator.java
@@ -3,12 +3,31 @@ package cn.binarywang.tools.generator;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
 import org.apache.commons.lang3.StringUtils;
 
+import com.google.common.collect.Maps;
+
+/**
+ * 身份证号码
+ * 1、号码的结构
+ * 公民身份号码是特征组合码，由十七位数字本体码和一位校验码组成。排列顺序从左至右依次为：六位数字地址码，
+ * 八位数字出生日期码，三位数字顺序码和一位数字校验码。
+ * 2、地址码(前六位数）
+ * 表示编码对象常住户口所在县(市、旗、区)的行政区划代码，按GB/T2260的规定执行。
+ * 3、出生日期码（第七位至十四位）
+ * 表示编码对象出生的年、月、日，按GB/T7408的规定执行，年、月、日代码之间不用分隔符。
+ * 4、顺序码（第十五位至十七位）
+ * 表示在同一地址码所标识的区域范围内，对同年、同月、同日出生的人编定的顺序号，
+ * 顺序码的奇数分配给男性，偶数分配给女性。
+ * 5、校验码（第十八位数）
+ * （1）十七位数字本体码加权求和公式 S = Sum(Ai * Wi), i = 0, ... , 16 ，先对前17位数字的权求和
+ * Ai:表示第i位置上的身份证号码数字值 Wi:表示第i位置上的加权因子 Wi: 7 9 10 5 8 4 2 1 6 3 7 9 10 5 8 4
+ * 2 （2）计算模 Y = mod(S, 11) （3）通过模得到对应的校验码 Y: 0 1 2 3 4 5 6 7 8 9 10 校验码: 1 0
+ * X 9 8 7 6 5 4 3 2
+ */
 public class IDCardNumberGenerator {
 
     public static String generate() {
@@ -48,7 +67,6 @@ public class IDCardNumberGenerator {
         String[] Wi = { "7", "9", "10", "5", "8", "4", "2", "1", "6", "3", "7",
             "9", "10", "5", "8", "4", "2" };
         int tmp = 0;
-        System.err.println(cardId);
         for (int i = 0; i < Wi.length; i++) {
             tmp += Integer.parseInt(String.valueOf(cardId.charAt(i)))
                 * Integer.parseInt(Wi[i]);
@@ -61,7 +79,7 @@ public class IDCardNumberGenerator {
     }
 
     private static Map<String, String> getAreaCode() {
-        final Map<String, String> map = new HashMap<>();
+        final Map<String, String> map = Maps.newHashMap();
         map.put("11", "北京");
         map.put("12", "天津");
         map.put("13", "河北");
